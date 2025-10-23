0a1140be9c738cbdd0b7492880372a8e956525e1
Nguyễn Thành Phúc <phucabc987@gmail.com>
2025-10-23 21:33:47 +0700

    Update frontend

--- Moji/backend/src/server.js
+++ Moji/backend/src/server.js
@@ -4,25 +4,28 @@ import { connectDB } from "./libs/db.js";
 import authRoute from "./routes/authRoute.js";
 import userRoute from "./routes/userRoute.js";
 import cookieParser from "cookie-parser";
-import { proctedRoute } from "./middlewares/authMiddleware.js";
+import { protectedRoute } from "./middlewares/authMiddleware.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-// middleware
+// middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 // public routes
 app.use("/api/auth", authRoute);
 
 // private routes
-app.use(proctedRoute);
+app.use(protectedRoute);
 app.use("/api/users", userRoute);
+
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`Server bắt đầu trên cổng ${PORT}`);
+    console.log(`server bắt đầu trên cổng ${PORT}`);
   });
 });
---
+++ Moji/frontend/src/components/auth/signup-form.tsx
@@ -0,0 +1,44 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+
+export function SignupForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form className="p-6 md:p-8">
+            <div className="flex flex-col gap-6">
+              {/* Header - logo */}
+
+              {/* Họ và tên */}
+
+              {/* Header - logo */}
+
+              {/* Header - logo */}
+
+              {/* Header - logo */}
+
+              {/* Header - logo */}
+            </div>
+          </form>
+          <div className="bg-muted relative hidden md:block">
+            <img
+              src="/placeholderSignUp.png"
+              alt="Image"
+              className="absolute top-1/2 -translate-y-1/2 object-cover"
+            />
+          </div>
+        </CardContent>
+      </Card>
+      <div className="px-6 text-center">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </div>
+    </div>
+  );
+}
